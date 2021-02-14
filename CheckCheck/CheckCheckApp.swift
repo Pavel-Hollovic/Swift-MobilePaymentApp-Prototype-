0f542e028bd9227566d6fd9047d37165f364031f
Pavel-Hollovic <pavelhollovic@gmail.com>
2021-02-14 18:06:57 +0100

    Initial Commit

---
+++ CheckCheck/CheckCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckCheckApp.swift
+//  CheckCheck
+//
+//  Created by Pavel Hollovic on 14.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct CheckCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
